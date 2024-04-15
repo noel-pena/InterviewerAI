6f2d236defbe74f4974ca1e05b1bfba97d8a9d3a
noel-pena <azulu88@hotmail.com>
2024-04-15 19:02:41 -0400

    init commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import OpenAI from "openai";
+
+dotenv.config();
+
+// eslint-disable-next-line no-undef
+const apiKey = process.env.OPENAI_API_KEY;
+
+const openai = new OpenAI({ apiKey });
+
+const runPrompt = async () => {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "Tell me a joke" }],
+    model: "gpt-3.5-turbo",
+    max_tokens: 2000,
+    temperature: 1,
+  });
+
+  console.log(completion.choices[0]);
+};
+
+runPrompt();
